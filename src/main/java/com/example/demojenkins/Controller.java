3224ae367b6fddf166878620a24fff028dc34018
Kevin Krisna <kevinkrisnadp98@gmail.com>
2023-01-10 12:59:37 +0700

    testing

---
+++ src/main/java/com/example/demojenkins/Controller.java
@@ -0,0 +1,13 @@
+package com.example.demojenkins;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("testing")
+    public String testing(){
+        return "test";
+    }
+}
--- src/main/java/com/example/demojenkins/DemoJenkinsApplication.java
+++ src/main/java/com/example/demojenkins/DemoJenkinsApplication.java
@@ -6,16 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class DemoJenkinsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoJenkinsApplication.class, args);
-    }
-
-    @GetMapping("testing")
-    public String testing(){
-        return "test";
     }
 
 }
